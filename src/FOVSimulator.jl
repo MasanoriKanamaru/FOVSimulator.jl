48eaa4c57777cff8461c25c21afa98107bc93deb
mkanamaru <kanamaru-masanori@hotmail.co.jp>
2024-10-31 06:21:05 +0900

    Update FOVSimulator.jl

--- src/FOVSimulator.jl
+++ src/FOVSimulator.jl
@@ -1,5 +1,17 @@
 module FOVSimulator
 
-# Write your package code here.
+using LinearAlgebra
+using Rotations
+using StaticArrays
 
-end
+import SPICE
+
+include("SpiceCamera.jl")
+export SpiceCamera, update!
+
+## Utility functions
+angle_rad(v1, v2) = @. acos(normalize(v1) ⋅ normalize(v2))
+angle_deg(v1, v2) = rad2deg.(angle_rad(v1, v2))
+export angle_rad, angle_deg
+
+end # module FOVSimulator
