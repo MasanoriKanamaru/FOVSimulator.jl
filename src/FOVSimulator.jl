7e702d98c7ba7d991311aae13e1bc5543ae392f6
mkanamaru <kanamaru-masanori@hotmail.co.jp>
2024-11-05 14:13:46 +0900

    Update FOVSimulator.jl
    Improve numerical stability

--- src/FOVSimulator.jl
+++ src/FOVSimulator.jl
@@ -10,11 +10,11 @@ include("SpiceCamera.jl")
 export SpiceCamera, update!
 
 ## Utility functions
-angle_rad(v1::AbstractVector{<:Number}, v2::AbstractVector{<:Number}) = acos(normalize(v1) ⋅ normalize(v2))
-angle_deg(v1::AbstractVector{<:Number}, v2::AbstractVector{<:Number}) = rad2deg(angle_rad(v1, v2))
+angle_rad(v1::AbstractVector{<:Real}, v2::AbstractVector{<:Real}) = acos(clamp(normalize(v1) ⋅ normalize(v2), -1.0, 1.0))
+angle_deg(v1::AbstractVector{<:Real}, v2::AbstractVector{<:Real}) = rad2deg(angle_rad(v1, v2))
 
-angle_rad(v1::AbstractVector{<:AbstractVector{<:Number}}, v2::AbstractVector{<:AbstractVector{<:Number}}) = angle_rad.(v1, v2)
-angle_deg(v1::AbstractVector{<:AbstractVector{<:Number}}, v2::AbstractVector{<:AbstractVector{<:Number}}) = angle_deg.(v1, v2)
+angle_rad(v1::AbstractVector{<:AbstractVector{<:Real}}, v2::AbstractVector{<:AbstractVector{<:Real}}) = angle_rad.(v1, v2)
+angle_deg(v1::AbstractVector{<:AbstractVector{<:Real}}, v2::AbstractVector{<:AbstractVector{<:Real}}) = angle_deg.(v1, v2)
 
 export angle_rad, angle_deg
 
