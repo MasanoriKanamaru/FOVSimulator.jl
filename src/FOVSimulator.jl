1349d9c38fba247e45520262047037749f993c51
mkanamaru <kanamaru-masanori@hotmail.co.jp>
2024-11-05 14:03:08 +0900

    Update FOVSimulator.jl
    Fix broadcasting in `angle_rad` and `angle_deg`.

--- src/FOVSimulator.jl
+++ src/FOVSimulator.jl
@@ -10,8 +10,12 @@ include("SpiceCamera.jl")
 export SpiceCamera, update!
 
 ## Utility functions
-angle_rad(v1, v2) = @. acos(normalize(v1) ⋅ normalize(v2))
-angle_deg(v1, v2) = rad2deg.(angle_rad(v1, v2))
+angle_rad(v1::AbstractVector{<:Number}, v2::AbstractVector{<:Number}) = acos(normalize(v1) ⋅ normalize(v2))
+angle_deg(v1::AbstractVector{<:Number}, v2::AbstractVector{<:Number}) = rad2deg(angle_rad(v1, v2))
+
+angle_rad(v1::AbstractVector{<:AbstractVector{<:Number}}, v2::AbstractVector{<:AbstractVector{<:Number}}) = angle_rad.(v1, v2)
+angle_deg(v1::AbstractVector{<:AbstractVector{<:Number}}, v2::AbstractVector{<:AbstractVector{<:Number}}) = angle_deg.(v1, v2)
+
 export angle_rad, angle_deg
 
 end # module FOVSimulator
